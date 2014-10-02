bcea91367a0eee3da5169fbb15813e00c7d2e04c
ThomasMoynihan <tommybhoy96@hotmail.com>
2014-10-02 13:48:58 +0100

    Prototype for Toast Simulator
    Library 2/10/14
    
    Thomas, Calum & Alan

---
+++ Toast Sim Prototype/Assets/Toast_object.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toast_object : MonoBehaviour {
+
+	void OnMouseDown() {
+		print ("qinnningsnidgnisnd");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
