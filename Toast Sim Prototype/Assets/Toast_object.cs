32f9f55485270cc2626fb5c675e301dc8bf6ec84
ThomasMoynihan <tommybhoy96@hotmail.com>
2014-10-02 14:45:35 +0100

    Toast Sim Prototype
    asdasd

---
+++ Toast Sim Prototype/Assets/Toast_object.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toast_object : MonoBehaviour {
+
+	int score = 0;
+
+
+	void OnMouseDown() {
+
+		score++;
+		print ("Score: " +score);
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Score_txt= score;
+	}
+}
---
+++ Toast Sim Prototype/Assets/main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class main : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
