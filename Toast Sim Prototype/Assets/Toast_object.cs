4fdeee13f39eb9f62dde509c86d59c0f149b0dd6
ThomasMoynihan <tommybhoy96@hotmail.com>
2014-10-05 16:37:13 +0100

    added timer
    With the Big Alinator

--- Toast Sim Prototype/Assets/Toast_object.cs
+++ Toast Sim Prototype/Assets/Toast_object.cs
@@ -4,24 +4,28 @@ using System.Collections;
 public class Toast_object : MonoBehaviour {
 
 	int score = 0;
-
+	public int toast = 0;
+	public float timeIncrease = 1;
+	public bool timeElapsed = false;
+	int toastseconds = 5 ;
 
 	void OnMouseDown() {
 
 		score++;
-		print ("Score: " +score);
-
-
+		print ("Score: " + score);
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-		Score_txt= score;
 	}
+
+
+
+
 }
--- Toast Sim Prototype/Assets/main.cs
+++ Toast Sim Prototype/Assets/main.cs
@@ -4,17 +4,30 @@ using System.Collections;
 
 
 public class main : MonoBehaviour {
-
-
+	int toastwait = 3;
 
 	// Use this for initialization
 	void Start () {
-	
+		print ("Starting: " + Time.time);
+		StartCoroutine(timer());
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	IEnumerator  timer() {
+
+
+		for (int toast = 0; toast < 5; toast++) {
+			print ("Toast: " + toast);
+			print ("Calum a is a huge cock");
+
+			yield return new WaitForSeconds(toastwait);
+		}
+
 	}
 
 
