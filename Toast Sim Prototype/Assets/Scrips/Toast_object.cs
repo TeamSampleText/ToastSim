cf72f41c6f7d2f512d3424216c1ecae6a61fecd3
gamereat <djyeats@gmail.com>
2014-10-16 17:00:19 +0100

    Got toasted made prototype

--- Toast Sim Prototype/Assets/Scrips/Toast_object.cs
+++ Toast Sim Prototype/Assets/Scrips/Toast_object.cs
@@ -3,31 +3,25 @@ using System.Collections;
 
 
 public class Toast_object : MonoBehaviour {
-	public GUIText score_txt;
+	
 	public GameObject toast;
-
+	public static bool toastClicked = false;
 	void OnMouseDown() {
-		main.score ++;
-		UpdateScore ();
-		print ("Current Score: " + main.score);
+		toastClicked = true;
 		Destroy (gameObject);
-	//	DestroyImmediate (toast, true);
-
+		
 	}
-
+	
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-		//score_txt.text = "Score " + score;
+		
 	}
-
-	void UpdateScore() {
-		score_txt.text = "Score " + main.score + "/" + Toaster.toastcount;
-	}
-
-
+	
+	
+	
 }
--- Toast Sim Prototype/Assets/Scrips/main.cs
+++ Toast Sim Prototype/Assets/Scrips/main.cs
@@ -4,68 +4,89 @@ using System.Collections;
 
 
 public class main : MonoBehaviour {
-	public static int score = 0;
+	public int score = 0;
 	public Transform Toast;
+	
 	public GameObject Toaster_obj; 
 	public GameObject toast_obj;
 	private float toastX = 0;
 	private float toastY = 0;
 	private int toastwait = 1;
 	private int timerToast = 0;
-	private int testtime = 5 ;
+	private int testtime = 2 ;
 	private bool toastMake = false;
+	public  GUIText score_txt;
 	// Use this for initialization
 	void Start () {
-
+		
 		//print ("Starting: " + Time.time);
-	
-
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 		StartCoroutine(timer());
 		toastX = 0;
 		toastY = 0.7f;
-
-
-	}
-
-	IEnumerator  timer() {
-
-		if(toastMake == false && Toaster.toasterclicked == true){
-
-
-		for (; timerToast < testtime; timerToast++) {
-
-			toastMake = true;
-
-			print ("Timer: " + timerToast);
 		
-			Toaster.toasterclicked = false;
-
-			//Pop toast
-
-			yield return new WaitForSeconds(toastwait);
-
-				}
-
-			print("X pos: "+Toaster_obj.transform.position.x + "y pos: "+ Toaster.ToasterY);
-			Toaster.toasterclicked = false;
+		if (Toast_object.toastClicked == true) {
+			//	print (currentToast.transform.position.y);
+		
+				//if(toast_obj.transform.position.y>Toaster_obj.transform.localPosition.y+1)
+				
+				score+=3;
+				
 
 			
-			Instantiate(Toast, new Vector3(toastX,toastY), Quaternion.identity);
+			Toast_object.toastClicked = false;
+			score_txt.text = "Score: " + score ;
+			//DestroyObject(toast_obj);
+			
+			
+		}
+	}
+	
+	IEnumerator  timer() {
+		
+		if(toastMake == false && Toaster.toasterclicked == true){
+			
+			
+			for (; timerToast < testtime; timerToast++) {
+				
+				toastMake = true;
+				
+				print ("Timer: " + timerToast);
+				
+				Toaster.toasterclicked = false;
+				
+				//Pop toast
+				
+				yield return new WaitForSeconds(toastwait);
+				
+			}
+			
+			print("X pos: "+Toaster_obj.transform.position.x + "y pos: "+ Toaster.ToasterY);
+			Toaster.toasterclicked = false;
+			
+			
+			Instantiate(Toast, new Vector3(toastX,toastY), Quaternion.identity) ;
 			//toast_obj.gameObject.tag = "Toast "+ toastCount;
 			//print("Toast made " + Toast.tag);
 			timerToast = 0;
 			toastMake = false;
-			}
-
 		}
-
+		
 	}
-
+	
+	
+	void UpdateScore() {
+		
+	}
+	
+	
+}
 
 
 
